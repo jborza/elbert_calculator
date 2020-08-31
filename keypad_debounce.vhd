770d82aaed5e90328eb6818c5804035902c2b075
jborza <jborza@gmail.com>
2020-08-31 22:37:54 +0200

    debounce - renamed next_ to prev_keypad_state

--- keypad_debounce.vhd
+++ keypad_debounce.vhd
@@ -17,18 +17,20 @@ architecture Behavioral of keypad_debounce is
 
 begin
 	process(clk, reset) 
-		constant DEBOUNCE_CLK_PERIODS : integer := 12_000_000 / 4; --we want it to stay the same for 1 ms
-		--constant DEBOUNCE_CLK_PERIODS : integer := 8;
-		variable next_keypad_state : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+		--constant DEBOUNCE_CLK_PERIODS : integer := 12_000_000 / 1; --we want it to stay the same for 1 ms
+		constant DEBOUNCE_CLK_PERIODS : integer := 8;
+		variable prev_keypad_state : STD_LOGIC_VECTOR(3 downto 0) := "0000";
 		variable debounce_count : integer range 0 to DEBOUNCE_CLK_PERIODS-1 := 0;
 	begin
 		if(reset = '1') then
 			debounce_count := 0;
 			output_stable <= '0';
+			key_pressed_debounced <= "0000";
+			prev_keypad_state := "0000";
 		elsif rising_edge(clk) then
-			if (key_pressed_in /= next_keypad_state) then
+			if (key_pressed_in /= prev_keypad_state) then
 				key_pressed_debounced <= "0000";
-				next_keypad_state := key_pressed_in;
+				prev_keypad_state := key_pressed_in;
 				debounce_count := 0;
 				output_stable <= '0';
 			else 
@@ -36,7 +38,7 @@ begin
 				  debounce_count := debounce_count + 1;
 				  output_stable <= '0';
 				else
-				  key_pressed_debounced <= next_keypad_state;
+				  key_pressed_debounced <= prev_keypad_state;
 				  output_stable <= '1';
 				end if;
 			end if;
